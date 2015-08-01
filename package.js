32df98ce5f55c7f68fdc828a07c3d0d2b5fcc755
Pete Corey <petecorey@gmail.com>
2015-08-01 14:44:26 -0700

    Removed package dependency

--- package.js
+++ package.js
@@ -6,10 +6,6 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Npm.depends({
-  'es6-promise': '2.3.0'
-});
-
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.addFiles('lib/wrap-promise.js');
