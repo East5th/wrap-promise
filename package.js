b5d1d1018a74ccd49a2ebbf06de1938114caa934
Pete Corey <petecorey@gmail.com>
2015-07-31 17:35:02 -0700

    First commit

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'wrap-promise',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('lib/wrap-promise.js');
+});
