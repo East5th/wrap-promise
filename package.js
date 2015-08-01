878ad587224be855d3cfc256e5e37672c86a950a
Pete Corey <petecorey@gmail.com>
2015-07-31 17:35:55 -0700

    Updated package.js

--- package.js
+++ package.js
@@ -1,16 +1,13 @@
 Package.describe({
-  name: 'wrap-promise',
+  name: 'east5th:wrap-promise',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Exposes the wrapPromise utility method',
+  git: 'https://github.com/East5th/wrap-promise',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
   api.addFiles('lib/wrap-promise.js');
+  api.export('wrapPromise');
 });
