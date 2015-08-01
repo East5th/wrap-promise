0f516ab0a8cca9b24f834cf66c0edd9675e3f791
Pete Corey <petecorey@gmail.com>
2015-08-01 14:48:42 -0700

    Incremented version to 0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'east5th:wrap-promise',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Exposes the wrapPromise utility method',
   git: 'https://github.com/East5th/wrap-promise',
   documentation: 'README.md'
